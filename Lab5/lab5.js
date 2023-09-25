9699a0ae06f2b33166b29949248a2bd46b2c159b
Ktran07 <143166209+Ktran07@users.noreply.github.com>
2023-09-24 22:45:41 -1000

    Add files via upload

---
+++ Lab5/lab5.js
@@ -0,0 +1,16 @@
+let age = 23
+let fav_num = 2
+let day_of_birth = 4
+let month_of_birth = 6
+
+let calc1 = age + fav_num / day_of_birth * month_of_birth
+let calc2 = (age + fav_num) / day_of_birth * month_of_birth
+
+
+document.getElementById("result1").innerHTML = calc1; 
+document.getElementById("result2").innerHTML = calc2; 
+
+
+
+
+
